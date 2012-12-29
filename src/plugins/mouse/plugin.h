e59172fa47ffd242875f2b833110e6b5da7d8aa0
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-12-29 23:54:56 +0100

    Mouse preflet mockup.
    Implement the mouse preflet mockup.

---
+++ src/plugins/mouse/plugin.h
@@ -0,0 +1,51 @@
+/****************************************************************************
+ * This file is part of System Preferences.
+ *
+ * Copyright (C) 2011-2012 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:LGPL2.1+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#ifndef PLUGIN_H
+#define PLUGIN_H
+
+#include <Hawaii/SystemPreferences/PreferencesModulePlugin>
+
+namespace Hawaii
+{
+    namespace SystemPreferences
+    {
+        class PreferencesModule;
+
+        class MousePlugin : public PreferencesModulePlugin
+        {
+            Q_OBJECT
+            Q_PLUGIN_METADATA(IID "org.hawaii.SystemPreferences.PreferencesModuleFactoryInterface" FILE "mouse.json")
+        public:
+            explicit MousePlugin(QObject *parent = 0);
+
+            virtual QStringList keys() const;
+            virtual PreferencesModule *create(const QString &key) const;
+        };
+    }
+}
+
+#endif // PLUGIN_H
---
+++ src/plugins/mouse/preflet.cpp
@@ -0,0 +1,69 @@
+/****************************************************************************
+ * This file is part of System Preferences.
+ *
+ * Copyright (C) 2011-2012 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:LGPL2.1+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#include "preflet.h"
+#include "ui_mousepreflet.h"
+
+using namespace Hawaii::SystemPreferences;
+
+Preflet::Preflet()
+    : PreferencesModule()
+    , ui(new Ui::MousePreflet)
+{
+    ui->setupUi(this);
+}
+
+Preflet::~Preflet()
+{
+    delete ui;
+}
+
+QString Preflet::name() const
+{
+    return tr("Mouse & Touchpad");
+}
+
+QString Preflet::comment() const
+{
+    return tr("Set your mouse and touchpad preferences");
+}
+
+QString Preflet::iconName() const
+{
+    return QLatin1String("input-mouse");
+}
+
+QStringList Preflet::keywords() const
+{
+    return tr("pointer;mouse;click;double;button;trackpad;trackball;touchpad;tap").split(";");
+}
+
+PreferencesModule::Category Preflet::category() const
+{
+    return PreferencesModule::HardwareCategory;
+}
+
+#include "moc_preflet.cpp"
