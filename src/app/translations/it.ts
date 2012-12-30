8c93102f9c9ed25308b7d7e3f8126c3f21482aea
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-12-30 02:37:03 +0100

    Add Italian translation.
    First translation.

---
+++ src/app/translations/it.ts
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="53"/>
+        <source>System Preferences</source>
+        <translation>Preferenze di sistema</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="58"/>
+        <source>Keywords</source>
+        <translation>Parole chiave</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="113"/>
+        <source>Overview</source>
+        <translation>Panoramica</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="121"/>
+        <source>Tool Bar</source>
+        <translation>Barra degli strumenti</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../menuitem.cpp" line="147"/>
+        <location filename="../menuitem.cpp" line="180"/>
+        <source>Personal</source>
+        <translation>Personale</translation>
+    </message>
+    <message>
+        <location filename="../menuitem.cpp" line="151"/>
+        <location filename="../menuitem.cpp" line="183"/>
+        <source>Hardware</source>
+        <translation>Hardware</translation>
+    </message>
+    <message>
+        <location filename="../menuitem.cpp" line="155"/>
+        <location filename="../menuitem.cpp" line="186"/>
+        <source>System</source>
+        <translation>Sistema</translation>
+    </message>
+    <message>
+        <location filename="../menuitem.cpp" line="159"/>
+        <location filename="../menuitem.cpp" line="189"/>
+        <source>Other</source>
+        <translation>Altro</translation>
+    </message>
+</context>
+</TS>
