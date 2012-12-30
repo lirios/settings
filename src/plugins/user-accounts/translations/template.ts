5ebaef12cc27c0c02e59764664e83e979f0bf624
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-12-30 03:09:29 +0100

    Preflets translation support.
    Add translations support to preflets.

---
+++ src/plugins/user-accounts/translations/template.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Hawaii::SystemPreferences::Preflet</name>
+    <message>
+        <location filename="../preflet.cpp" line="75"/>
+        <source>Users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../preflet.cpp" line="80"/>
+        <source>Create new users, change users&apos; details such as real name and icon, manage administrators and groups</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../preflet.cpp" line="91"/>
+        <source>user;login;account;face;password;administrator;name;fingerprint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../preflet.cpp" line="112"/>
+        <source>Standard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../preflet.cpp" line="115"/>
+        <source>Administrator</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UsersPreflet</name>
+    <message>
+        <location filename="../userspreflet.ui" line="111"/>
+        <source>Click to change user&apos;s name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userspreflet.ui" line="114"/>
+        <source>$NAME$</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userspreflet.ui" line="146"/>
+        <source>Account type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userspreflet.ui" line="156"/>
+        <source>$TYPE$</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userspreflet.ui" line="163"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userspreflet.ui" line="178"/>
+        <source>Login Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userspreflet.ui" line="186"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userspreflet.ui" line="202"/>
+        <source>Click to change user&apos;s password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="../userspreflet.ui" line="205"/>
+        <source>●●●●</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userspreflet.ui" line="217"/>
+        <source>Automatic Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
