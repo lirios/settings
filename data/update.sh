f66ab7c7825ba058083d149fe3cf6b52be7bc337
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2013-09-15 20:17:27 +0200

    Replace desktop entry update script with a new one
    Write a new script in Python that produces a better output and discards
    translations for the Icon entry.

--- data/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-basefilename=hawaii-system-preferences
-cat ${basefilename}.desktop.in > ${basefilename}.desktop
-grep --no-filename '\[.*]\s*=' ${basefilename}_*.desktop >> ${basefilename}.desktop
