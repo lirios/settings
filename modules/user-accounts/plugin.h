b9182dd9336c66d24846dca388abe4a3ffe82074
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2016-01-20 05:59:13 +0100

    Update copyright year
    Also reformat the notice.

--- modules/user-accounts/plugin.h
+++ modules/user-accounts/plugin.h
@@ -1,10 +1,10 @@
 /****************************************************************************
  * This file is part of System Preferences.
  *
- * Copyright (C) 2011-2013 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ * Copyright (C) 2011-2016 Pier Luigi Fiorini
  *
  * Author(s):
- *    Pier Luigi Fiorini
+ *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
  *
  * $BEGIN_LICENSE:LGPL2.1+$
  *
