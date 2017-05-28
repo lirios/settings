28e5ec8db86aab07d3ed39a92d4fa8d805badbea
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2017-05-28 11:59:42 +0200

    Update users translation

--- modules/users/translations/users_pl.ts
+++ modules/users/translations/users_pl.ts
@@ -1,5 +1,12 @@
 <?xml version="1.0" ?><!DOCTYPE TS><TS language="pl" version="2.1">
 <context>
+    <name>PhotoPicker</name>
+    <message>
+        <source>Browse...</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
     <name>Preflet</name>
     <message>
         <source>Your Account</source>
@@ -8,6 +15,123 @@
     <message>
         <source>Other Accounts</source>
         <translation>Inne konta</translation>
+    </message>
+</context>
+<context>
+    <name>UserAddDialog</name>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Account type</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Standard</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Administrator</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Full name</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>User name</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Allow user to set a password at the next login</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Set a password now</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>No password</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>New password</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Verify new password</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>UserListItem</name>
+    <message>
+        <source>Do you want to keep %1&apos;s files?</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>It is possible to keep the home directory, mail spool and temporary files around when deleting a user account.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Remove user</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>UserPage</name>
+    <message>
+        <source>Full name</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Account type</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Standard</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Administrator</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Automatic login</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>New password</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Verify new password</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Change</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Wrong password verification</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>The passwords did not match.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Password changed</source>
+        <translation type="unfinished"/>
     </message>
 </context>
 </TS>
