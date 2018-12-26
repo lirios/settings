e27f39281e36d7fdd7576e46a138052190861ed5
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-12-26 12:51:49 +0100

    Search libcrypt manually
    Arch Linux doesn't have the pkg-config file like Fedora, libcrypt is
    provided by glibc there.

--- cmake/FindLibcrypt.cmake
+++ cmake/FindLibcrypt.cmake
@@ -1,3 +1,28 @@
-find_package(PkgConfig)
+find_path(Libcrypt_INCLUDE_DIR NAMES "crypt.h")
+find_library(Libcrypt_LIBRARY NAMES "crypt")
 
-pkg_check_modules(Libcrypt libcrypt REQUIRED IMPORTED_TARGET)
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(
+    Libcrypt
+    FOUND_VAR
+        Libcrypt_FOUND
+    REQUIRED_VARS
+        Libcrypt_LIBRARY
+        Libcrypt_INCLUDE_DIR
+)
+
+if(Libcrypt_FOUND AND NOT TARGET Libcrypt::Libcrypt)
+    add_library(Libcrypt::Libcrypt UNKNOWN IMPORTED)
+    set_target_properties(Libcrypt::Libcrypt PROPERTIES
+        IMPORTED_LOCATION "${Libcrypt_LIBRARY}"
+        INTERFACE_COMPILE_OPTIONS "${Libcrypt_DEFINITIONS}"
+        INTERFACE_INCLUDE_DIRECTORIES "${Libcrypt_INCLUDE_DIR}"
+    )
+endif()
+
+mark_as_advanced(Libcrypt_LIBRARY Libcrypt_INCLUDE_DIR)
+
+# Compatibility
+set(Libcrypt_LIBRARIES ${Libcrypt_LIBRARY})
+set(Libcrypt_INCLUDE_DIRS ${Libcrypt_INCLUDE_DIR})
