da821b58b0b6c4e36c136286fdfbb2147fd6161e
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2019-06-02 10:22:17 +0200

    Make region translatable

---
+++ modules/region/translations/locale.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>FormatsDialog</name>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="44"/>
+        <source>Formats</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="74"/>
+        <source>Search a language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="95"/>
+        <location filename="../contents/FormatsDialog.qml" line="120"/>
+        <location filename="../contents/FormatsDialog.qml" line="134"/>
+        <location filename="../contents/FormatsDialog.qml" line="148"/>
+        <source>%1 (%2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="112"/>
+        <source>Dates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="126"/>
+        <source>Times</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="140"/>
+        <source>Dates and Times</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="154"/>
+        <source>Numbers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="168"/>
+        <source>Measurement</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="182"/>
+        <source>Metric</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="184"/>
+        <source>Imperial (UK)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="186"/>
+        <source>Imperial (US)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="188"/>
+        <source>Imperial</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="193"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/FormatsDialog.qml" line="198"/>
+        <source>Currency</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LanguageDialog</name>
+    <message>
+        <location filename="../contents/LanguageDialog.qml" line="41"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/LanguageDialog.qml" line="69"/>
+        <source>Search a language</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../contents/main.qml" line="52"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="55"/>
+        <location filename="../contents/main.qml" line="56"/>
+        <location filename="../contents/main.qml" line="66"/>
+        <location filename="../contents/main.qml" line="67"/>
+        <source>%1 (%2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../contents/main.qml" line="63"/>
+        <source>Formats</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
