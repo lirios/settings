20193ab97c65624100760ca6e6c10470abbb69af
Liri CI <ci@liri.io>
2021-12-08 00:47:25 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- translations/modules/locale/locale_nl.ts
+++ translations/modules/locale/locale_nl.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="nl">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="nl" version="2.1">
 <context>
     <name>FormatsDialog</name>
     <message>
